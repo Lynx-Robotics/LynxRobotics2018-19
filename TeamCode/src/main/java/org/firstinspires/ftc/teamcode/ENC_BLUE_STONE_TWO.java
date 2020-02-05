0c78c1276963efbd1f026e1f21d2ebbafa68cb85
subash0793 <54967684+subash0793@users.noreply.github.com>
2020-02-05 10:28:41 -0600

    Red stone two readjusted -- V2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ENC_BLUE_STONE_TWO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ENC_BLUE_STONE_TWO.java
@@ -15,11 +15,7 @@ public class ENC_BLUE_STONE_TWO extends autoBaseV2 {
             chart.init(hardwareMap);
 
             waitForStart();
-
-            //strafe Right
-            //goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(5), -0.3);
-
-            //go forward
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
             goToPosition(chart.elevMotor, 360, 1.0);
             goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNewFullVolt(27.0), 1.0);
 
@@ -32,127 +28,117 @@ public class ENC_BLUE_STONE_TWO extends autoBaseV2 {
 
             //strafe left
             chart.runtime.reset();
-            strafe(0.30);
+            strafe(0.50);
             while(opModeIsActive() && (!SkyStoneReBornRight(chart.colorSensorRight))){
 
             }
             rest();
             goToPositionDown(chart.elevMotor, 5, -1.0);
-
-
-            goToPositionStrafeRight(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(6), 0.30);
-
+            goToPositionStrafeRight(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(2.5), 0.50);
 
             //go forward
-            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(4), 0.4);
+            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(4), 0.45);
 
             //grab
             dropDL();
-            sleep(1500);
 
+            sleep(500);
             //raise elevMotor
             goToPosition(chart.elevMotor, 13, 1.0);
-
             //go back
-            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(12.5), -7);
-
-
+            goToPositionAnti(chart.BR, chart.TL, distance2encoderNew(2), -0.4, false);
+            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(13), -7);
             rest();
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
             //goToPosition(chart.BL,chart.TR,distance2encoder(38),0.4,false);
-            goToPositionStrafeBackLeft( chart.TL,chart.TR, distance2encoderNew(32), 0.30);
+            goToPositionStrafeBackLeft( chart.TL,chart.TR, distance2encoderNew(32), 0.40);
             rest();
-            goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(2), -0.3);
+            goToPositionAnti(chart.BR, chart.TL, distance2encoderNew(2), -0.6, false);
+            goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(4), -0.6);
 
-            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(1), -7);
-            rest();
-            goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(1), -0.3);
+//            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(1), -7);
+//            rest();
+//            goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(2), -0.6);
+//            rest();
+            goToPositionAnti(chart.BR, chart.TL, distance2encoderNew(4), -0.6, false);
             goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(10), 1);
             rest();
-            goForward(0.4);
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+            goForward(0.45);
             while(opModeIsActive() && (!bottomTapeSensorDetectedBlueReborn(chart.bottomColorSensor))){
 
             }
             rest();
 
-            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(10), 1);
+            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(7), 1);
 
             rest();
 
+            raiseDL();
+            goToPosition(chart.elevMotor, 14, 1.0);
+            goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(2.5), -0.5);
+            goToPositionAnti(chart.BR, chart.TL, distance2encoderNew(2), -0.6, false);
+            sleep(200);
+           // sleep(450);
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //goToPosition(chart.elevMotor, 10, 1.0);
+
+//            goForward(-0.45);
+//            while(opModeIsActive() && (!bottomTapeSensorDetectedBlueReborn(chart.bottomColorSensor))){
+//
+//            }
+
+            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(80), -1.0);
+            rest();
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            goToPositionStrafeBackLeft( chart.TR,chart.TL, distance2encoderNew(30), 0.45);
+            rest();
+            goToPosition(chart.elevMotor, 360, 1.0);
+            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNewFullVolt(7), 1);
+            rest();
+          //  goToPosition(chart.BR, chart.TL, distance2encoderNew(0.3), 1.0, false);
+            sleep(450);
+            goToPositionAnti(chart.BR, chart.TL, distance2encoderNew(4), -0.6, false);
+            //strafe left
+            chart.runtime.reset();
+            strafe(-0.50);
+            while(opModeIsActive() && (!SkyStoneReBornRight(chart.colorSensorRight))){
+
+            }
+            rest();
+            goToPositionDown(chart.elevMotor, 5, -1.0);
+            goToPositionStrafeRight(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(4), 0.30);
+           //go forward
+            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(4), 0.4);
+            //grab
+            dropDL();
+            sleep(1500);
+            //raise elevMotor
+            goToPosition(chart.elevMotor, 13, 1.0);
+            //go back
+            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(13), -7);
+            rest();
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            goToPosition(chart.BL,chart.TR,distance2encoder(39),0.4,false);
+            rest();
+            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(1), -7);
+            rest();
+            goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(1), -0.3);
+            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(20), 1);
+            rest();
+            goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(2), -0.3);
+            rest();
+
+            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(40), 1);
             raiseDL();
             sleep(450);
 
             goToPosition(chart.elevMotor, 10, 1.0);
 
             goForward(-0.4);
-            while(opModeIsActive() && (!bottomTapeSensorDetectedBlueReborn(chart.bottomColorSensor))){
-
-            }
-
-            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(20), -1.0);
-            rest();
-            goToPositionStrafeBackLeft( chart.TR,chart.TL, distance2encoderNew(30), 0.30);
-            rest();
-
-
-            goToPosition(chart.elevMotor, 360, 1.0);
-            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNewFullVolt(7.5), 0.4);
-
-            rest();
-
-
-            goToPosition(chart.BR, chart.TL, distance2encoderNew(0.3), 1.0, false);
-
-            sleep(450);
-
-            //strafe left
-            chart.runtime.reset();
-            strafe(-0.30);
-            while(opModeIsActive() && (!SkyStoneReBornRight(chart.colorSensorRight))){
-
-            }
-            rest();
-            goToPositionDown(chart.elevMotor, 5, -1.0);
-
-
-            goToPositionStrafeRight(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(4), 0.30);
-
-
-            //go forward
-            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(4), 0.4);
-
-            //grab
-            dropDL();
-            sleep(1500);
-
-            //raise elevMotor
-            goToPosition(chart.elevMotor, 13, 1.0);
-
-            //go back
-            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(12), -7);
-
-            //strafe left more until foundation
-            //goToPositionNinety(chart.BL,chart.TR,distance2encoderNew(8),1,chart.TL,chart.BR,distance2encoderNew(5),-1,false);
-            rest();
-            goToPosition(chart.BL,chart.TR,distance2encoder(38),0.4,false);
-            rest();
-
-            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(1), -7);
-            rest();
-            goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(1), -0.3);
-            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(10), 1);
-            rest();
-            goForward(0.4);
             while(opModeIsActive() && (!bottomTapeSensorDetectedBlueReborn(chart.bottomColorSensor)));
-            rest();
-            goToPositionStrafeLeft(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(1), -0.3);
-            rest();
-            goToPosition(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(10), 1);
-            raiseDL();
-            sleep(450);
-
-            goToPosition(chart.elevMotor, 10, 1.0);
-
-            goToPositionBack(chart.TL, chart.TR, chart.BL, chart.BR, distance2encoderNew(10), -1.0);
             rest();
 
         }
