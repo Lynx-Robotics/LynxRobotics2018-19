bb3c375621e8e13b6ec01beb4587991bc2513826
AlphaLocke <47842170+AlphaLocke@users.noreply.github.com>
2020-02-17 08:11:02 -0600

    OFF-Season CODE

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imuReadOut.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class imuReadOut extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+    }
+}
